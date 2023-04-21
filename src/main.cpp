3122e49a940f43f46d5f16e7c707fe61104b6d77
Huo1ong <quentin.guillou49@gmail.com>
2023-04-21 11:21:17 -0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "Adafruit_NeoPixel.h"
+
+#define LED_PIN 12
+#define LED_COUNT 10
+#define LED_BRIGHTNESS 50
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  strip.begin();
+  strip.setBrightness(LED_BRIGHTNESS);
+  strip.show();
+}
+
+void loop() {
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(255, 0, 0));
+    strip.show();
+    delay(100);
+  }
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(0, 255, 0));
+    strip.show();
+    delay(100);
+  }
+  for (int i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, strip.Color(0, 0, 255));
+    strip.show();
+    delay(100);
+  }
+}
